3fcc19550ea29466201c03eef8016436e0f13c66
TiesDO <denouden.ties@gmail.com>
2024-07-05 00:18:27 +0200

    move player

--- sources/main.c
+++ sources/main.c
@@ -3,12 +3,15 @@
 #define SCREEN_WIDTH (800)
 #define SCREEN_HEIGHT (450)
 
+#define STD_MOVE_SPEED (5)
+
 #define WINDOW_TITLE "My Pong"
 
 typedef struct Paddle {
   int pos_x;
   int pos_y;
   int height;
+	int move_spd;
 } t_paddle;
 
 typedef struct Ball {
@@ -21,12 +24,14 @@ void init_player(t_paddle *p) {
   p->pos_x = 20;
   p->pos_y = 200;
   p->height = 60;
+	p->move_spd = STD_MOVE_SPEED;
 }
 
 void init_opponent(t_paddle *p) {
   p->pos_x = SCREEN_WIDTH - 20;
   p->pos_y = 200;
   p->height = 60;
+	p->move_spd = STD_MOVE_SPEED;
 }
 
 void init_ball(t_ball *b) {
@@ -36,6 +41,27 @@ void init_ball(t_ball *b) {
 
 void draw_paddle(t_paddle *p) {
   DrawRectangle(p->pos_x, p->pos_y, 10, p->height, WHITE);
+}
+
+void process_movement(t_paddle *p) {
+  bool key_up = IsKeyDown(KEY_UP);
+  bool key_down = IsKeyDown(KEY_DOWN);
+
+  if (key_up && key_down) {
+		return;
+  } else if (key_up) {
+		if (p->pos_y - p->move_spd < 0) {
+			p->pos_y = 0;
+		} else {
+			p->pos_y -= p->move_spd;
+		}
+  } else if (key_down) {
+		if (p->pos_y + p->height + p->move_spd > SCREEN_HEIGHT) {
+			p->pos_y = SCREEN_HEIGHT - p->height;
+		} else {
+			p->pos_y += p->move_spd;
+		}
+  }
 }
 
 int main(void) {
@@ -50,6 +76,7 @@ int main(void) {
   init_ball(&ball);
 
   while (!WindowShouldClose()) {
+    process_movement(&player);
 
     // Drawing
     BeginDrawing();
